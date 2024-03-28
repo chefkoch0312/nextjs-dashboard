03a7968117ecb0559fa8aaccbc0450e4fa95c681
Kai Dombrowski <kai.dombrowski@web.de>
2024-03-28 15:59:03 +0100

    test

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -20,7 +20,6 @@ export default async function Page() {
                 Dashboard
             </h1>
             <p>test</p>
-            <blubb />
             <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
                 <Card title="Collected" value={totalPaidInvoices} type="collected" />
                 <Card title="Pending" value={totalPendingInvoices} type="pending" />
